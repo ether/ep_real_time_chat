6038db253644693795368643ba8e0218488b3611
John McLear <john@mclear.co.uk>
2013-04-15 03:08:12 +0100

    first working version

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,18 @@
 exports.postAceInit = function(hook_name, args, cb) {
-  enable();
+  // Write the settings checkbox to the UI
+  $('#chattext').css("top", "45px");
+  $('#titlebar').append("<span style='padding:5px;float:left;'><input type=checkbox id='enableRealTimeChat'>&nbsp;<label for=enableRealTimeChat>Send Real Time Chat Updates</label></span>");
+
+  $('#enableRealTimeChat').click(function(){
+    $('#chatinput').focus();
+  });
+
+  // Listen for key ups in the chat input box
+  $('#chatinput').keyup(function(){
+    if($('#enableRealTimeChat').is(':checked')) {
+      sendChat();
+    }
+  });
 };
 
 exports.getAuthorClassName = function(author)
@@ -11,26 +24,6 @@ exports.getAuthorClassName = function(author)
   });
 }
 
-exports.className2Author = function(className)
-{
-  if (className.substring(0, 15) == "ep_real_time_chat-")
-  {
-    return className.substring(15).replace(/[a-y0-9]+|-|z.+?z/g, function(cc)
-    {
-      if (cc == '-') return '.';
-      else if (cc.charAt(0) == 'z')
-      {
-        return String.fromCharCode(Number(cc.slice(1, -1)));
-      }
-      else
-      {
-        return cc;
-      }
-    });
-  }
-  return null;
-}
-
 exports.handleClientMessage_CUSTOM = function(hook, context, cb){
   var action = context.payload.action;
   var padId = context.payload.padId;
@@ -38,21 +31,30 @@ exports.handleClientMessage_CUSTOM = function(hook, context, cb){
   var message = context.payload.message;
   var authorName = context.payload.authorName;
   var authorClass = exports.getAuthorClassName(authorId);
+  var authorClassCSS = authorClass.replace("ep_real_time_chat","author");
 
   if(pad.getUserId() === authorId) return false; // Dont process our own caret position (yes we do get it..) -- This is not a bug
 
-  if(action === 'recieveChatMessage'){ // an author has sent this client a chat position, we need to show it in the dom
+  if(action === 'recieveChatMessage'){ // an author has sent this client a chat message update, we need to show it in the dom
 
     var authorName = decodeURI(escape(context.payload.authorName));
     if(authorName == "null"){
       var authorName = "Anonymous" // If the users username isn't set then display a smiley face
     }
-    $('.authorChatMessage-'+authorClass).remove();
-    var html = '<p class="authorChatMessage-'+authorClass+'" id="authorChatMessage-' +authorClass+ '">' + authorName + ': ' + message + '</p>'
-    if( $('.authorChatMessage-'+authorClass).length === 0 ){ // if a container doesnt exist for this author yet
-      $('#chattext').append(html); // create new html
+    $('#authorChatMessage-'+authorClass).remove();
+    var html = '<p class="'+authorClassCSS+'" id="authorChatMessage-' +authorClass+ '"><b>' + authorName + '</b>: ' + message + '...</p>'
+    if( $('#authorChatMessage-'+authorClass).length === 0 ){ // if a container doesnt exist for this author yet
+      if(message){
+        $('#chattext').append(html); // create new html
+        parent.chat.scrollDown();
+      }
     }else{
-      $('.authorChatMessage-'+authorClass).html(html); // update html value
+      if(message){
+        $('#authorChatMessage-'+authorClass).html(html); // update html value
+        parent.chat.scrollDown();
+      }else{
+        $('#authorChatMessage-'+authorClass).remove();
+      } 
     }
   }
 }
@@ -69,19 +71,5 @@ function sendChat(){
     padId : padId,
     myAuthorId : myAuthorId
   }
-  //console.log("sent", message);
   pad.collabClient.sendMessage(message);  // Send the chat position message to the server
-}
-
-function enable(){
-  $('#chattext').css("top", "45px");
-  $('#titlebar').append("<span style='padding:5px;float:left;'><input type=checkbox id='enableRealTimeChat'>&nbsp;<label for=enableRealTimeChat>Send Real Time Chat Updates</label></span>");
-
-  $('#enableRealTimeChat').click(function(){
-    
-  });
-
-  $('#chatinput').keyup(function(){
-    sendChat();
-  });
 }
