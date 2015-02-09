defd3ac323e79b43cabeb7be5686f44eaaf7abe6
John McLear <john@mclear.co.uk>
2015-02-09 00:17:20 +0000

    styling

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,7 @@ exports.postAceInit = function(hook_name, args, cb) {
 
   // Write the settings checkbox to the UI
   $('#chattext').css("top", "45px");
-  $('#titlebar').append("<span style='width:100%;padding:5px;float:left;'><input type=checkbox id='enableRealTimeChat'>&nbsp;<label for=enableRealTimeChat>Send Real Time Chat Updates</label></span>");
+  $('#titlebar').append("<span style='width:100%;padding:5px 0px 5px 0px;float:left;'><input style='margin-left:5px' type=checkbox id='enableRealTimeChat'>&nbsp;<label for=enableRealTimeChat>Send Real Time Chat Updates</label></span>");
 
   $('#enableRealTimeChat').click(function(){
     $('#chatinput').focus();
